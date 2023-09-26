1536919edd9639f4918a779f49d8e9ed1a348fb6
antonioglass <71437038+antonioglass@users.noreply.github.com>
2023-09-26 12:01:59 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ rm -rf /workspace/venv
 
 echo "Cloning A1111 repo to /workspace"
 cd /workspace
-git clone --depth=1 https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+git clone --depth=1 https://github.com/antonioglass/stable-diffusion-webui.git
 
 echo "Installing Ubuntu updates"
 apt update
@@ -25,7 +25,7 @@ echo "Installing xformers"
 pip install --no-cache-dir xformers
 
 echo "Installing A1111 Web UI"
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/install-automatic.py
+wget https://raw.githubusercontent.com/antonioglass/runpod-worker-a1111/main/install-automatic.py
 python -m install-automatic --skip-torch-cuda-test
 
 echo "Cloning ControlNet extension repo"
@@ -37,27 +37,23 @@ pip install -r requirements.txt
 
 echo "Installing RunPod Serverless dependencies"
 cd /workspace/stable-diffusion-webui
-pip3 install huggingface_hub runpod
+pip3 install huggingface_hub runpod>=0.10.0
 
 echo "Downloading Deliberate v2 model"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget -O deliberate_v2.safetensors https://civitai.com/api/download/models/15236
+wget -O general_v3.safetensors https://civitai.com/api/download/models/156110
 
 echo "Downloading ControlNet models"
 mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
 cd /workspace/stable-diffusion-webui/models/ControlNet
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
-wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
+wget https://civitai.com/api/download/models/44811 --content-disposition
 
 echo "Downloading Upscalers"
+mkdir -p /workspace/stable-diffusion-webui/models/ESRGAN
 cd /workspace/stable-diffusion-webui/models/ESRGAN
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
+wget https://huggingface.co/antonioglass/upscalers/resolve/main/4x-AnimeSharp.pth
+wget https://huggingface.co/antonioglass/upscalers/resolve/main/4x_NMKD-Siax_200k.pth
+wget https://huggingface.co/antonioglass/upscalers/resolve/main/8x_NMKD-Superscale_150000_G.pth
 
 echo "Creating log directory"
 mkdir -p /workspace/logs
@@ -65,9 +61,9 @@ mkdir -p /workspace/logs
 echo "Installing config files"
 cd /workspace/stable-diffusion-webui
 rm webui-user.sh config.json ui-config.json
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/webui-user.sh
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/config.json
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/ui-config.json
+wget https://raw.githubusercontent.com/antonioglass/runpod-worker-a1111/main/webui-user.sh
+wget https://raw.githubusercontent.com/antonioglass/runpod-worker-a1111/main/config.json
+wget https://raw.githubusercontent.com/antonioglass/runpod-worker-a1111/main/ui-config.json
 
 echo "Starting A1111 Web UI"
 deactivate

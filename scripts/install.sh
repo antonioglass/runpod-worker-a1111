dc301cfcbbfc93ab27bd84df6c1bfffe781e4c99
antonioglass <71437038+antonioglass@users.noreply.github.com>
2024-02-01 17:01:25 +0200

    updated installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,34 +19,57 @@ python -m venv /workspace/venv
 source /workspace/venv/bin/activate
 
 echo "Installing Torch"
-pip install --no-cache-dir torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install --no-cache-dir torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 echo "Installing xformers"
-pip install --no-cache-dir xformers==0.0.22
+pip3 install --no-cache-dir xformers==0.0.22
 
 echo "Installing A1111 Web UI"
 wget https://raw.githubusercontent.com/antonioglass/runpod-worker-a1111/main/install-automatic.py
 python -m install-automatic --skip-torch-cuda-test
 
 echo "Cloning ControlNet extension repo"
+cd /workspace/stable-diffusion-webui
 git clone https://github.com/antonioglass/sd-webui-controlnet.git extensions/sd-webui-controlnet
 
+echo "Cloning the ReActor extension repo"
+git clone --depth=1 https://github.com/Gourieff/sd-webui-reactor.git extensions/sd-webui-reactor
+
 echo "Installing dependencies for ControlNet"
-cd extensions/sd-webui-controlnet
-pip install -r requirements.txt
+cd /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
+pip3 install -r requirements.txt
+
+echo "Installing dependencies for ReActor"
+cd /workspace/stable-diffusion-webui/extensions/sd-webui-reactor
+pip3 install protobuf==3.20.3
+pip3 install -r requirements.txt
+pip3 install onnxruntime-gpu==1.16.3
+
+echo "Installing the model for ReActor"
+mkdir -p /workspace/stable-diffusion-webui/models/insightface
+cd /workspace/stable-diffusion-webui/models/insightface
+wget https://huggingface.co/antonioglass/reactor/resolve/main/inswapper_128.onnx
+
+echo "Configuring ReActor to use the GPU instead of CPU"
+echo "CUDA" > /workspace/stable-diffusion-webui/extensions/sd-webui-reactor/last_device.txt
 
 echo "Installing RunPod Serverless dependencies"
 cd /workspace/stable-diffusion-webui
-pip3 install huggingface_hub runpod>=0.10.0
+pip3 install huggingface_hub runpod
 
 echo "Downloading Stable Diffusion models"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget https://civitai.com/api/download/models/197181 --content-disposition
+wget https://huggingface.co/antonioglass/models/resolve/main/3dAnimationDiffusion_v10.safetensors
+wget https://huggingface.co/antonioglass/models/resolve/main/epicphotogasm_y.safetensors
+wget https://huggingface.co/antonioglass/models/resolve/main/general_v3.safetensors
+wget https://huggingface.co/antonioglass/models/resolve/main/meinahentai_v4.safetensors
+wget https://huggingface.co/antonioglass/models/resolve/main/semi-realistic_v6.safetensors
 
 echo "Downloading ControlNet models"
 mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
 cd /workspace/stable-diffusion-webui/models/ControlNet
-wget https://civitai.com/api/download/models/44811 --content-disposition
+wget https://huggingface.co/antonioglass/controlnet/resolve/main/controlnet11Models_openpose.safetensors
+wget https://huggingface.co/antonioglass/controlnet/raw/main/controlnet11Models_openpose.yaml
 
 echo "Downloading Upscalers"
 mkdir -p /workspace/stable-diffusion-webui/models/ESRGAN
@@ -54,6 +77,39 @@ cd /workspace/stable-diffusion-webui/models/ESRGAN
 wget https://huggingface.co/antonioglass/upscalers/resolve/main/4x-AnimeSharp.pth
 wget https://huggingface.co/antonioglass/upscalers/resolve/main/4x_NMKD-Siax_200k.pth
 wget https://huggingface.co/antonioglass/upscalers/resolve/main/8x_NMKD-Superscale_150000_G.pth
+
+echo "Downloading Embeddings"
+mkdir -p /workspace/stable-diffusion-webui/embeddings
+cd /workspace/stable-diffusion-webui/embeddings
+wget https://huggingface.co/antonioglass/embeddings/resolve/main/BadDream.pt
+wget https://huggingface.co/antonioglass/embeddings/resolve/main/FastNegativeV2.pt
+wget https://huggingface.co/antonioglass/embeddings/resolve/main/UnrealisticDream.pt
+
+echo "Downloading Loras"
+mkdir -p /workspace/stable-diffusion-webui/models/Lora
+cd /workspace/stable-diffusion-webui/models/Lora
+wget https://huggingface.co/antonioglass/loras/resolve/main/EkuneCowgirl.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/EkunePOVFellatioV2.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/EkuneSideDoggy.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/IPV1.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/JackOPoseFront.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/LickingOralLoRA.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/POVAssGrab.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/POVDoggy.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/POVMissionary.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/POVPaizuri.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/POVReverseCowgirl.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/PSCowgirl.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/RSCongress.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/SelfBreastGrab.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/SideFellatio.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/TheMating.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/cuddling_handjob_v0.1b.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/hand_in_panties_v0.82.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/jkSmallBreastsLite_V01.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/masturbation_female.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/shirtliftv1.safetensors
+wget https://huggingface.co/antonioglass/loras/resolve/main/yamato_v2.safetensors
 
 echo "Creating log directory"
 mkdir -p /workspace/logs
